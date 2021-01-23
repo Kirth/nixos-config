2d56e550eb18f07df57d61a47d17f79f64685cab
Kirth <lammens@fitanalytics.com>
2021-01-23 20:13:29 +0100

    fix: networkmanager dont work with userControlled

--- laptop.nix
+++ laptop.nix
@@ -19,7 +19,7 @@ in
 
     wireless.enable = true;
     wireless.userControlled.enable = true;
-    networkmanager.enable = true;
+#    networkmanager.enable = true;
     wireless.networks = (import (./wifi.nix) { inherit pkgs; });
   };
 
